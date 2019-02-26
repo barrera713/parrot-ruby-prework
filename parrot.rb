33bea0de9a39ebc4c38b036ffe58cfb0b9afe619
Carlos Barrera <barrera9696@gmail.com>
2019-02-26 23:39:52 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,8 @@
-# Create method `parrot` that outputs a given phrase and
+def parrot(phrase = "Squawk!")
+  
+  
+  
+  
+  
+  # Create method `parrot` that outputs a given phrase and
 # returns the phrase
